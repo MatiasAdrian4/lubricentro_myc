b4b61f3888342e49aff6d09cb2d1b1baaa24ffd6
MatiasAdrian4 <matiasadrian4@gmail.com>
2019-07-14 19:14:14 -0300

    created Cliente and Producto models

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('lubricentro_myc/', include('lubricentro_myc.urls')),
+    #path('lubricentro_myc/', include('lubricentro_myc.urls')),
 ]
--- django_project/lubricentro_myc/models.py
+++ django_project/lubricentro_myc/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=100, blank=True, default='')
+    localidad = models.CharField(max_length=100, blank=True, default='')
+    codigo_postal = models.IntegerField(max_length=4, blank=true, default=None)
+    telefono = models.IntegerField(blank=true, default=None)
+    cuit = models.CharField(max_length=13, blank=True, default='')
+
+    class __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    codigo = models.IntegerField(primary_key=True)
+    detalle = models.CharField(max_length=200)
+    stock = models.IntegerField(blank=True, default=0)
+    precio_costo = models.DoubleField(blank=True, default=0.0)
+    precio_venta_contado = models.DoubleField(blank=True, default=0.0)
+    precio_venta_cta_cte = models.DoubleField(blank=True, default=0.0)
+    categoria = models.CharField(max_length=50)
+
+    class __str__(self):
+        return self.detalle
+
