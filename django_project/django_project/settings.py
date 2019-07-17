952466402501f654cae1a8fc91008d82f28a71b3
MatiasAdrian4 <matiasadrian4@gmail.com>
2019-07-17 07:41:42 -0300

    changed database to mysql

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'lubricentro_myc',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
