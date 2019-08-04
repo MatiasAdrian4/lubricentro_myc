c30a47aa8afe8427eb23003de09f1e0df356ee57
MatiasAdrian4 <matiasadrian4@gmail.com>
2019-08-04 19:43:39 -0300

    added render_to_pdf function

--- django_project/lubricentro_myc/urls.py
+++ django_project/lubricentro_myc/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
-from lubricentro_myc.views import ClienteViewSet, ProductoViewSet, InventarioView
+from lubricentro_myc.views import ClienteViewSet, ProductoViewSet, InventarioView, generate_stock_pdf
 
 router = DefaultRouter()
 #router.register(r'clientes', ClienteViewSet)
@@ -10,6 +10,10 @@ urlpatterns = [
     url(r'inventario/', 
         InventarioView.as_view(),
         name='inventario'
+    ),
+    url(r'generate_stock_pdf/',
+        generate_stock_pdf,
+        name='generate_stock_pdf'
     ),
     url('', include(router.urls))
 ]
--- django_project/lubricentro_myc/views.py
+++ django_project/lubricentro_myc/views.py
@@ -1,9 +1,12 @@
-from django.http import Http404
+from django.http import Http404, HttpResponse
+from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.template.loader import get_template
 from rest_framework import viewsets
 
 from .models import Cliente, Producto
 from .serializers import ClienteSerializer, ProductoSerializer
+from .utils import render_to_pdf
 
 class ClienteViewSet(viewsets.ModelViewSet):
     queryset = Cliente.objects.all()
@@ -42,3 +45,28 @@ class InventarioView(ListView):
         categorias = Producto.objects.order_by().values('categoria').distinct()
         context['categorias'] = categorias
         return context
+
+def generate_stock_pdf(request, *args, **kwargs):
+    categoria = request.GET['categoria']
+    if categoria is not None:
+        productos = Producto.objects.filter(categoria=categoria)
+    else:
+        productos = Producto.objects.none()
+
+    template = get_template('stock.html')
+    context = {
+        "categoria" : categoria,
+        "productos" : productos
+    }
+    html = template.render(context)
+    pdf = render_to_pdf('stock.html', context)
+    if pdf:
+        response = HttpResponse(pdf, content_type='application/pdf')
+        filename = 'stock_%s.pdf' %(categoria)
+        content = "inline; filename='%s'" %(filename)
+        download = request.GET.get("download")
+        if download:
+            content = "attachment; filename=%s" %(filename)
+        response['Content-Disposition'] = content
+        return response
+    return HttpResponse("Not found")
