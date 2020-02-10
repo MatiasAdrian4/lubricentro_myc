124576fcb51aa38d81a6f4047a317349eab07ca7
MatiasAdrian4 <matiasadrian4@gmail.com>
2020-02-10 17:05:12 -0300

    added payment indication in remitos section

---
+++ django_project/lubricentro_myc/templatetags/custom_tags.py
@@ -0,0 +1,12 @@
+from django import template
+register = template.Library()
+
+from lubricentro_myc.models import ElementoRemito
+
+@register.simple_tag
+def check_pagado(request, remito_id):
+    elem_remitos = ElementoRemito.objects.filter(remito=remito_id, pagado=False)
+    if len(elem_remitos) == 0:
+        return True
+    else:
+        return False
