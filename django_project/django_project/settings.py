96cfad239eeea78bf410c9a495682e5c73346949
MatiasAdrian4 <matiasadrian4@gmail.com>
2020-03-07 15:59:53 -0300

    fixed problem with static_root

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -137,7 +137,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 STATIC_URL = '/static/'
 
