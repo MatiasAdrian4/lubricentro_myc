ea1a4a54a8a8ef113f5b68c7c9a9027e20834ccf
Matias Adrian Penalba Paz <matiasadrian4@Matiass-MacBook-Air.local>
2022-04-29 08:29:38 -0300

    LMYC-1: moved views to a separate folder

---
+++ django_project/lubricentro_myc/views/user.py
@@ -0,0 +1,16 @@
+import json
+
+from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
+
+
+@require_http_methods(["POST"])
+def crear_usuario(request):
+    data = json.loads(request.body.decode('utf-8'))
+    user = User.objects.create_user(
+        username=data['nombre'],
+        password=data['password'],
+        email=data['email'],
+    )
+    user.save()
+    return HttpResponse(status=200)
