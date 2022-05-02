cb8f105d21a60749732e665a8a10de34532b42d4
Matias Adrian Penalba Paz <matiasadrian4@Matiass-MacBook-Air.local>
2022-05-02 08:35:50 -0300

    LMYC-5: created new field codigo_en_pantalla for starting to fix the problem with the edit products functionality

--- django_project/lubricentro_myc/models.py
+++ django_project/lubricentro_myc/models.py
@@ -16,7 +16,8 @@ class Cliente(models.Model):
 
 
 class Producto(models.Model):
-    codigo = models.IntegerField(primary_key=True)
+    codigo = models.AutoField(primary_key=True)
+    codigo_en_pantalla = models.IntegerField(null=True, unique=True)
     detalle = models.CharField(max_length=200)
     stock = models.FloatField(default=0.0)
     precio_costo = models.FloatField(default=0.0)  # sin iva
