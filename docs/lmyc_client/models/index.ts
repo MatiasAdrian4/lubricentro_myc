f84e841e072b4fa91e825770f1fa49069f155b25
Matias Adrian Penalba Paz <matiasadrian4@Matiass-MacBook-Air.local>
2022-05-21 10:22:18 -0300

    LMYC-18: generated swagger client

---
+++ docs/lmyc_client/models/index.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Client';
+export * from './ClientSearchResultList';
+export * from './Invoice';
+export * from './InvoiceItem';
+export * from './InvoiceItemQuantityList';
+export * from './InvoiceItemsIdsList';
+export * from './InvoiceItemsList';
+export * from './InvoiceItemsQuantityList';
+export * from './Product';
+export * from './ProductList';
+export * from './ProductUpdateCost';
+export * from './ProductUpdateCostResult';
+export * from './Sale';
+export * from './SaleList';
+export * from './SalesPerMonth';
+export * from './SalesPerYear';
+export * from './SimplifiedClient';
