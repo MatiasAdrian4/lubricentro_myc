728419e46b6d3b1b9ebe1c2f33b1742658e3dcfc
Matias Adrian Penalba Paz <matiasadrian4@Matiass-MacBook-Air.local>
2022-05-21 19:08:48 -0300

    LMYC-20: added swagger for account endpoints

--- django_project/lubricentro_myc/urls.py
+++ django_project/lubricentro_myc/urls.py
@@ -15,10 +15,10 @@ router.register(r"elementos_remito", ElementoRemitoViewSet)
 router.register(r"ventas_realizadas", VentaViewSet)
 
 urlpatterns = [
-    re_path(r"signup/", SignupView.as_view()),
-    re_path(r"login/", LoginView.as_view()),
-    re_path(r"user/", UserView.as_view()),
-    re_path(r"logout/", LogoutView.as_view()),
+    re_path(r"account/signup/", SignupView.as_view()),
+    re_path(r"account/login/", LoginView.as_view()),
+    re_path(r"account/user/", UserView.as_view()),
+    re_path(r"account/logout/", LogoutView.as_view()),
     re_path(r"generar_remito_pdf/", generar_remito_pdf, name="generar_remito_pdf"),
     re_path(r"generar_stock_pdf/", generar_stock_pdf, name="generar_stock_pdf"),
     re_path("", include(router.urls)),
