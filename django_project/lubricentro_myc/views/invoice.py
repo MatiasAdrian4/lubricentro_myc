8083c3bce1b011368800041284a88ce57e5847b6
Matias Adrian Penalba Paz <matiasadrian4@Matiass-MacBook-Air.local>
2022-06-05 19:54:26 -0300

    LMYC-30: invoices endpoint now returns client name instead client id

--- django_project/lubricentro_myc/views/invoice.py
+++ django_project/lubricentro_myc/views/invoice.py
@@ -10,7 +10,7 @@ from rest_framework.decorators import action
 
 
 class RemitoViewSet(viewsets.ModelViewSet):
-    queryset = Remito.objects.all().order_by("codigo")
+    queryset = Remito.objects.all().order_by("-fecha")
     serializer_class = RemitoSerializer
 
     @action(detail=False, methods=["get"])
