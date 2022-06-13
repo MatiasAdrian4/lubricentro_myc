6b760b0be68bd2a403aa44f7f00b5f637b8d5d39
Matias Adrian Penalba Paz <matiasadrian4@Matiass-MacBook-Air.local>
2022-06-12 21:09:19 -0300

    LMYC-33: updated unit tests

--- django_project/lubricentro_myc/signals.py
+++ django_project/lubricentro_myc/signals.py
@@ -7,21 +7,20 @@ from lubricentro_myc.models.product import Producto
 @receiver(pre_save, sender=ElementoRemito)
 def save_invoice_item(sender, instance, **kwargs):
     new_quantity = None
-    older = instance
-    if older.id:  # an existent invoice item is being updated
-        newer = ElementoRemito.objects.get(id=instance.id)
-        if older.cantidad != newer.cantidad:
-            new_quantity = older.cantidad - newer.cantidad
+    if instance.id:  # an existent invoice item is being updated
+        older_instance = ElementoRemito.objects.get(id=instance.id)
+        if instance.cantidad != older_instance.cantidad:
+            new_quantity = instance.cantidad - older_instance.cantidad
     else:  # a new invoice item is being created
-        new_quantity = older.cantidad
+        new_quantity = instance.cantidad
     if new_quantity:
-        producto = Producto.objects.get(codigo=older.producto.codigo)
-        producto.stock -= new_quantity
-        producto.save()
+        product = Producto.objects.get(codigo=instance.producto.codigo)
+        product.stock -= new_quantity
+        product.save()
 
 
 @receiver(post_delete, sender=ElementoRemito)
 def delete_invoice_item(sender, instance, **kwargs):
-    producto = Producto.objects.get(codigo=instance.producto.codigo)
-    producto.stock += instance.cantidad
-    producto.save()
+    product = Producto.objects.get(codigo=instance.producto.codigo)
+    product.stock += instance.cantidad
+    product.save()
---
+++ django_project/lubricentro_myc/tests/test_signals.py
@@ -0,0 +1,50 @@
+from django.test import TestCase
+from lubricentro_myc.models.product import Producto
+from lubricentro_myc.signals import delete_invoice_item, save_invoice_item
+from lubricentro_myc.tests.factories import (
+    ClientFactory,
+    InvoiceFactory,
+    InvoiceItemFactory,
+    ProductFactory,
+)
+
+
+class SignalsTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.client_1 = ClientFactory()
+        cls.product_1 = ProductFactory(stock=5.0)
+        cls.product_2 = ProductFactory(stock=11.5)
+        cls.invoice = InvoiceFactory(cliente=cls.client_1)
+
+    def test_save_invoice_item_with_new_invoice_item(self):
+        new_invoice_item = InvoiceItemFactory.build(
+            remito=self.invoice, producto=self.product_1, cantidad=4.0
+        )
+        save_invoice_item(sender=None, instance=new_invoice_item)
+        product = Producto.objects.get(codigo=self.product_1.codigo)
+        self.assertEqual(product.stock, 1.0)
+
+    def test_save_invoice_item_updating_invoice_item(self):
+        invoice_item_1 = InvoiceItemFactory(
+            remito=self.invoice, producto=self.product_1, cantidad=0.0
+        )
+        new_invoice_item = InvoiceItemFactory.build(
+            id=invoice_item_1.id,
+            remito=self.invoice,
+            producto=self.product_1,
+            cantidad=3.0,
+        )
+        save_invoice_item(sender=None, instance=new_invoice_item)
+        product = Producto.objects.get(codigo=self.product_1.codigo)
+        self.assertEqual(product.stock, 2.0)
+
+    def test_delete_invoice_item(self):
+        invoice_item = InvoiceItemFactory.build(
+            remito=self.invoice,
+            producto=self.product_1,
+            cantidad=3.0,
+        )
+        delete_invoice_item(sender=None, instance=invoice_item)
+        product = Producto.objects.get(codigo=self.product_1.codigo)
+        self.assertEqual(product.stock, 8.0)
