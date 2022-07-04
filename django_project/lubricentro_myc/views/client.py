f591d7d1468ae3206b42bce3f7caba003e4bb046
Matias Adrian Penalba Paz <matiasadrian4@Matiass-MacBook-Air.local>
2022-07-03 22:55:00 -0300

    LMYC-43: added SingleClienteSerializer

--- django_project/lubricentro_myc/views/client.py
+++ django_project/lubricentro_myc/views/client.py
@@ -1,11 +1,18 @@
 from lubricentro_myc.models.client import Cliente
-from lubricentro_myc.serializers.client import ClienteSerializer
+from lubricentro_myc.serializers.client import (
+    ClienteSerializer,
+    SingleClienteSerializer,
+)
 from rest_framework import viewsets
 
 
 class ClienteViewSet(viewsets.ModelViewSet):
     queryset = Cliente.objects.all().order_by("id")
     serializer_class = ClienteSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        self.serializer_class = SingleClienteSerializer
+        return super().retrieve(request)
 
     def list(self, request):
         nombre = request.GET.get("nombre", None)
