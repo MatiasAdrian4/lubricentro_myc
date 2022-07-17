eab32f1610055fedf91a8ffd554e118d8e4fd32f
Matias Adrian Penalba Paz <matiasadrian4@Matiass-MacBook-Air.local>
2022-07-17 09:59:58 -0300

    LMYC-47: modified sales endpoint response to return product detail

--- django_project/lubricentro_myc/serializers/sale.py
+++ django_project/lubricentro_myc/serializers/sale.py
@@ -7,6 +7,14 @@ class VentaSerializer(serializers.ModelSerializer):
         model = Venta
         fields = "__all__"
 
+    def to_representation(self, instance):
+        data = super(VentaSerializer, self).to_representation(instance)
+        data["producto"] = {
+            "codigo": instance.producto.codigo,
+            "detalle": instance.producto.detalle,
+        }
+        return data
+
 
 class VentasSerializer(serializers.Serializer):
     ventas = serializers.ListField(child=VentaSerializer())
