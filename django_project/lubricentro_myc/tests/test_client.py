1c2b5c658023e33340657b797b1ca52052e5f342
Matias Adrian Penalba Paz <matiasadrian4@Matiass-MacBook-Air.local>
2022-08-26 07:57:50 -0300

    LMYC-64: added query param for searching in clients endpoint

--- django_project/lubricentro_myc/tests/test_client.py
+++ django_project/lubricentro_myc/tests/test_client.py
@@ -11,14 +11,30 @@ class ClientTestCase(TestCase):
     def setUpTestData(cls):
         cls.api_client = APIClient()
         cls.client_url = "/lubricentro_myc/clientes"
-        cls.client_1 = ClientFactory(nombre="Jose Gomez")
-        cls.client_2 = ClientFactory(nombre="Juan Perez")
-        cls.client_3 = ClientFactory(nombre="Maria Fernandez")
-        cls.client_4 = ClientFactory(nombre="Jose Hernandez")
+        cls.client_1 = ClientFactory(id=1, nombre="Jose Gomez")
+        cls.client_2 = ClientFactory(id=15, nombre="Juan Perez")
+        cls.client_3 = ClientFactory(id=30, nombre="Maria Fernandez")
+        cls.client_4 = ClientFactory(id=45, nombre="Jose Hernandez")
 
     @mock_auth
     def test_search_client(self):
         response = self.client.get(f"{self.client_url}?nombre=jose", follow=True)
+        clientes = json.loads(response.content)["results"]
+        self.assertEqual(len(clientes), 2)
+        self.assertEqual(clientes[0]["nombre"], "Jose Gomez")
+        self.assertEqual(clientes[1]["nombre"], "Jose Hernandez")
+
+    @mock_auth
+    def test_search_client_using_query_param_to_search_by_id(self):
+        response = self.client.get(f"{self.client_url}?query=1", follow=True)
+        clientes = json.loads(response.content)["results"]
+        self.assertEqual(len(clientes), 2)
+        self.assertEqual(clientes[0]["id"], 1)
+        self.assertEqual(clientes[1]["id"], 15)
+
+    @mock_auth
+    def test_search_client_using_query_param_to_search_by_name(self):
+        response = self.client.get(f"{self.client_url}?query=jose", follow=True)
         clientes = json.loads(response.content)["results"]
         self.assertEqual(len(clientes), 2)
         self.assertEqual(clientes[0]["nombre"], "Jose Gomez")
