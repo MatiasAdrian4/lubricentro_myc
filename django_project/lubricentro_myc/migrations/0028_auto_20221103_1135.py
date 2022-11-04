7dee0b1da54ba91211ab7fa4fc7cce06f94ddbdc
Matias Adrian Penalba Paz <matiasadrian4@Matiass-MacBook-Air.local>
2022-11-03 21:17:56 -0300

    LMYC-96: fix

--- django_project/lubricentro_myc/migrations/0028_auto_20221103_1135.py
+++ django_project/lubricentro_myc/migrations/0028_auto_20221103_1135.py
@@ -3,14 +3,21 @@
 from django.db import migrations
 
 
+def reset_product_code_auto_increment(apps, schema_editor):
+    with schema_editor.connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT oid FROM pg_class where relname='lubricentro_myc_producto_codigo_seq'"
+        )
+        if cursor.fetchone():
+            cursor.execute(
+                "ALTER SEQUENCE lubricentro_myc_producto_codigo_seq RESTART WITH 100000"
+            )
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
         ("lubricentro_myc", "0027_productpricehistory"),
     ]
 
-    operations = [
-        migrations.RunSQL(
-            "ALTER SEQUENCE lubricentro_myc_producto_codigo_seq RESTART WITH 100000;"
-        )
-    ]
+    operations = [migrations.RunPython(reset_product_code_auto_increment)]
