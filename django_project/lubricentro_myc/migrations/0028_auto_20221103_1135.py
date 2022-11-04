b63bb3bfa628b3b3d324e61a4408fd8eae5ee45c
Matias Adrian Penalba Paz <matiasadrian4@Matiass-MacBook-Air.local>
2022-11-03 22:59:16 -0300

    LMYC-96: use a const

--- django_project/lubricentro_myc/migrations/0028_auto_20221103_1135.py
+++ django_project/lubricentro_myc/migrations/0028_auto_20221103_1135.py
@@ -2,15 +2,17 @@
 
 from django.db import migrations
 
+PRODUCT_CODE_SEQUENCE_NAME = "lubricentro_myc_producto_codigo_seq"
+
 
 def reset_product_code_auto_increment(apps, schema_editor):
     with schema_editor.connection.cursor() as cursor:
         cursor.execute(
-            "SELECT oid FROM pg_class where relname='lubricentro_myc_producto_codigo_seq'"
+            f"SELECT oid FROM pg_class where relname='{PRODUCT_CODE_SEQUENCE_NAME}'"
         )
         if cursor.fetchone():
             cursor.execute(
-                "ALTER SEQUENCE lubricentro_myc_producto_codigo_seq RESTART WITH 100000"
+                f"ALTER SEQUENCE {PRODUCT_CODE_SEQUENCE_NAME} RESTART WITH 100000"
             )
 
 
