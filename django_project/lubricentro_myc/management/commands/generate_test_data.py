b76566c5c943877c950972b35270704e91fc6dd4
Matias Adrian Penalba Paz <matiasadrian4@Matiass-MacBook-Air.local>
2023-03-10 16:12:41 -0300

    LMYC-107: small refactor

--- django_project/lubricentro_myc/management/commands/generate_test_data.py
+++ django_project/lubricentro_myc/management/commands/generate_test_data.py
@@ -10,14 +10,11 @@ class Command(BaseCommand):
     help = "Stores mock data in the DB"
 
     def store_mock_users(self):
-        try:
-            User.objects.get(username="test_user")
-            logger.info("0 users were created.")
-        except User.DoesNotExist:
-            User.objects.create_user(
-                username="test_user", email="", password="test_password"
-            )
-            logger.info("1 user was created.")
+        User.objects.filter(username="test_user").delete()
+        User.objects.create_user(
+            username="test_user", email="", password="test_password"
+        )
+        logger.info("1 user was created.")
 
     def store_mock_products(self):
         logger.info("0 products were created.")
