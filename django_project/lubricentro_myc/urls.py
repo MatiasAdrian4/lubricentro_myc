aeb5d42cfa4db73a84d08d579e0f8a2af2d6d459
Matias Adrian Peñalba Paz <matiasadrianpp4@gmail.com>
2024-04-10 08:06:49 -0300

    add export models backup endpoint

--- django_project/lubricentro_myc/urls.py
+++ django_project/lubricentro_myc/urls.py
@@ -1,6 +1,10 @@
 from django.urls import include, re_path
 
-from lubricentro_myc.views.admin import activity_details, activities_list
+from lubricentro_myc.views.admin import (
+    activity_details,
+    activities_list,
+    export_models_backup,
+)
 from lubricentro_myc.views.client import ClienteViewSet
 from lubricentro_myc.views.db import reset
 from lubricentro_myc.views.file import generar_remito_pdf, generar_stock_pdf
@@ -33,6 +37,11 @@ urlpatterns = [
         name="activity-details",
     ),
     re_path(r"admin/activities/", activities_list, name="activities-list"),
+    re_path(
+        r"admin/export_models_backup/",
+        export_models_backup,
+        name="export-models-backup",
+    ),
     re_path("", include(router.urls)),
 ]
 
--- django_project/lubricentro_myc/views/admin.py
+++ django_project/lubricentro_myc/views/admin.py
@@ -1,9 +1,27 @@
+import csv
+import json
+import os
+import traceback
+import zipfile
+
 from django.http import HttpResponse, JsonResponse
+from django.utils import timezone
+
 from rest_framework.decorators import permission_classes, api_view
 from rest_framework.permissions import IsAuthenticated
 
-from lubricentro_myc.models import Activity
+from lubricentro_myc.models import (
+    Activity,
+    Producto,
+    ProductPriceHistory,
+    Cliente,
+    Venta,
+    Remito,
+    ElementoRemito,
+)
+from lubricentro_myc.models.activity import EXCEPTION, INFO
 from lubricentro_myc.serializers.activity import ActivitySerializer
+from lubricentro_myc.utils import log_activity
 
 
 @api_view(["GET"])
@@ -23,3 +41,46 @@ def activity_details(_, activity_id):
 
     serializer = ActivitySerializer(activity)
     return JsonResponse(data=serializer.data)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def export_models_backup(request):
+    start_activity = log_activity(
+        request, INFO, "Models Backup Export", json.dumps(request.data)
+    )
+    try:
+        response = HttpResponse(content_type="application/zip")
+        response["Content-Disposition"] = (
+            f'attachment; filename="models_backup_{timezone.now()}.zip"'
+        )
+
+        with zipfile.ZipFile(response, "w") as zipf:
+            models_to_export = [
+                ("products", Producto.objects.all()),
+                ("product_prices_history", ProductPriceHistory.objects.all()),
+                ("clients", Cliente.objects.all()),
+                ("sales", Venta.objects.all()),
+                ("invoices", Remito.objects.all()),
+                ("invoice_items", ElementoRemito.objects.all()),
+            ]
+
+            for filename, queryset in models_to_export:
+                f = open(f"{filename}.csv", "w")
+                writer = csv.writer(f, delimiter="|")
+                for obj in queryset:
+                    writer.writerow(obj.data)
+
+                zipf.write(f"{filename}.csv", arcname=f"{filename}.csv")
+                os.remove(f"{filename}.csv")
+
+        return response
+    except:
+        log_activity(
+            request,
+            EXCEPTION,
+            "Models Backup Export Failed",
+            traceback.format_exc(),
+            start_activity,
+        )
+        return HttpResponse(status=500)
