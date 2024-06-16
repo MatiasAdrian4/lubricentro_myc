39a6f1e0ab04db39e45d6d10dc1aaea62d8583bf
Matias Adrian Peñalba Paz <matiasadrianpp4@gmail.com>
2024-06-16 09:37:18 -0300

    create account_summary_item model

---
+++ django_project/lubricentro_myc/models/account_summary_item.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+DEBE = "debe"
+HABER = "haber"
+
+TYPE_CHOICES = [
+    (DEBE, "Debe"),
+    (HABER, "Haber"),
+]
+
+
+class AccountSummaryItem(models.Model):
+    client = models.ForeignKey("lubricentro_myc.Cliente", on_delete=models.CASCADE)
+    date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    amount = models.FloatField(null=False)
